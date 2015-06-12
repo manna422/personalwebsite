1a1eb7790bc907867ffab0905f1f5dd190a368e3
Nick Manna <manna422@gmail.com>
2015-06-12 04:11:42 -0400

    flask blog added

---
+++ initdb.sh
@@ -0,0 +1,7 @@
+sqlite3 ./blog.db << EOF
+create table entries (id integer primary key autoincrement,
+date text not null,
+time text not null,
+title text not null,
+'text' text not null);
+EOF
--- server.py
+++ server.py
@@ -1,12 +1,26 @@
-import os
+from os import path
+from sqlite3 import dbapi2 as sqlite3
 from flask import (Flask, request, session, g, redirect, url_for, abort,
                    render_template, flash)
 
 app = Flask(__name__)
 
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = sqlite3.connect(path.join(app.root_path, 'blog.db'))
+    return g.sqlite_db
+
+
 @app.route('/')
 def show_home():
-    return render_template('index.html')
+    db = get_db()
+    cur = db.execute('select date, time, title, text from entries order by id desc')
+    raw = cur.fetchall()
+    entries = []
+    for entry in raw:
+	entries.append({'date':entry[0], 'time':entry[1], 'title':entry[2],
+                         'text':entry[3]})
+    return render_template('index.html', entries=entries)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
